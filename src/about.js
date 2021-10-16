1adeee4bda9329f416ffa8aeba9bc32d95ebcacf
Carlos Alves <carlosalves369m@hotmail.com>
2021-10-16 09:04:31 -0300

    new: Add about

---
+++ src/about.js
@@ -0,0 +1,65 @@
+const ABOUT = `
+    <div class="container center">
+        <div class="content about">
+            <div id="image">
+                <img src="https://via.placeholder.com/400x593" alt="Image about">
+                <div id="describe-about">
+                    <h1>CONTACT US</h1>
+                    <p>
+                        Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed 
+                        non enim. Maecenas lacinia non orci at aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus 
+                        sapien, ut euismod leo tortor ac enim. In hac habitasse platea dictumst. Vestibulum eu quam nec neque pellentesque 
+                        efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed non enim. Maecenas lacinia non orci at 
+                        aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus sapien, ut euismod leo tortor ac enim. 
+                        In hac habitasse platea dictumst. Sed cursus venenatis tellus, non lobortis diam volutpat sit amet. Sed tellus augue, 
+                        hendrerit eu rutrum in, porttitor at metus. Mauris ac hendrerit metus. Phasellus mattis lectus commodo felis egestas, id 
+                        accumsan justo ultrices. Phasellus aliquet, sem a placerat dapibus, enim purus dictum lacus, nec ultrices ante dui ac 
+                        ante. Phasellus placerat, urna.
+                        Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed 
+                        non enim. Maecenas lacinia non orci at aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus 
+                        sapien, ut euismod leo tortor ac enim. In hac habitasse platea dictumst. Vestibulum eu quam nec neque pellentesque 
+                        efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed non enim. Maecenas lacinia non orci at 
+                        aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus sapien, ut euismod leo tortor ac enim. 
+                        In hac habitasse platea dictumst. Sed cursus venenatis tellus, non lobortis diam volutpat sit amet. Sed tellus augue, 
+                        hendrerit eu rutrum in, porttitor at metus. Mauris ac hendrerit metus. Phasellus mattis lectus commodo felis egestas, id 
+                        accumsan justo ultrices.
+                    </p>
+                    <div id="social-midia">
+                        <a href="https:/carlosalves.now.sh/" target="blank">
+                            <svg width="35" height="35" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M30.6199 17.3804C36.2212 22.9875 36.1443 31.9771 30.6537 37.4982C30.6434 37.5094 30.6312 37.5216 30.6199 37.5329L24.3199 43.8329C18.7634 49.3894 9.72314 49.3887 4.16742 43.8329C-1.38914 38.2773 -1.38914 29.236 4.16742 23.6804L7.64611 20.2017C8.56861 19.2792 10.1573 19.8924 10.2049 21.196C10.2657 22.8575 10.5636 24.5267 11.1134 26.1386C11.2996 26.6844 11.1665 27.2882 10.7587 27.696L9.5318 28.9229C6.90436 31.5503 6.82195 35.8285 9.42352 38.4816C12.0508 41.1609 16.3691 41.1768 19.0165 38.5294L25.3165 32.2304C27.9594 29.5875 27.9483 25.3157 25.3165 22.6838C24.9695 22.3375 24.62 22.0685 24.347 21.8805C24.1539 21.7479 23.9944 21.572 23.8813 21.3668C23.7683 21.1616 23.7047 20.9328 23.6957 20.6987C23.6586 19.708 24.0096 18.6872 24.7924 17.9044L26.7662 15.9305C27.2838 15.4129 28.0958 15.3493 28.696 15.7682C29.3833 16.2482 30.0271 16.7876 30.6199 17.3804ZM43.8326 4.16713C38.2769 -1.38867 29.2366 -1.38942 23.6801 4.16713L17.3801 10.4671C17.3688 10.4784 17.3566 10.4906 17.3463 10.5018C11.8558 16.0229 11.7788 25.0125 17.3801 30.6196C17.9728 31.2123 18.6166 31.7518 19.3039 32.2317C19.9041 32.6506 20.7162 32.5869 21.2337 32.0694L23.2075 30.0955C23.9903 29.3127 24.3413 28.2919 24.3042 27.3012C24.2952 27.0671 24.2316 26.8383 24.1186 26.6331C24.0055 26.4279 23.846 26.252 23.6529 26.1194C23.3799 25.9314 23.0304 25.6624 22.6834 25.3161C20.0516 22.6842 20.0405 18.4124 22.6834 15.7695L28.9834 9.47047C31.6308 6.82306 35.9491 6.839 38.5764 9.51828C41.178 12.1714 41.0956 16.4496 38.4681 19.077L37.2412 20.3039C36.8334 20.7117 36.7004 21.3155 36.8866 21.8613C37.4363 23.4732 37.7342 25.1424 37.795 26.8039C37.8427 28.1076 39.4313 28.7207 40.3538 27.7982L43.8325 24.3195C49.3891 18.764 49.3892 9.72275 43.8326 4.16713Z" fill="white"/>
+                            </svg>
+                        </a>
+                        <a href="https://www.instagram.com/carlos.alves/" target="blank">
+                            <svg width="35" height="35" viewBox="0 0 43 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M21.5048 10.2317C15.4037 10.2317 10.4826 15.0395 10.4826 21C10.4826 26.9605 15.4037 31.7683 21.5048 31.7683C27.6059 31.7683 32.527 26.9605 32.527 21C32.527 15.0395 27.6059 10.2317 21.5048 10.2317ZM21.5048 28.0008C17.5621 28.0008 14.3389 24.8612 14.3389 21C14.3389 17.1388 17.5525 13.9992 21.5048 13.9992C25.4571 13.9992 28.6707 17.1388 28.6707 21C28.6707 24.8612 25.4475 28.0008 21.5048 28.0008ZM35.5487 9.79125C35.5487 11.1877 34.3976 12.3029 32.9779 12.3029C31.5485 12.3029 30.407 11.1783 30.407 9.79125C30.407 8.40422 31.5581 7.27959 32.9779 7.27959C34.3976 7.27959 35.5487 8.40422 35.5487 9.79125ZM42.8489 12.3404C42.6858 8.9759 41.8992 5.99565 39.3763 3.54022C36.863 1.08479 33.8124 0.3163 30.3686 0.147607C26.8192 -0.0492023 16.1808 -0.0492023 12.6314 0.147607C9.19716 0.306928 6.14663 1.07542 3.6237 3.53085C1.10078 5.98628 0.323759 8.96653 0.151088 12.331C-0.0503625 15.7986 -0.0503625 26.192 0.151088 29.6596C0.314166 33.0241 1.10078 36.0044 3.6237 38.4598C6.14663 40.9152 9.18756 41.6837 12.6314 41.8524C16.1808 42.0492 26.8192 42.0492 30.3686 41.8524C33.8124 41.6931 36.863 40.9246 39.3763 38.4598C41.8896 36.0044 42.6762 33.0241 42.8489 29.6596C43.0504 26.192 43.0504 15.808 42.8489 12.3404ZM38.2635 33.3802C37.5153 35.2171 36.0668 36.6323 34.177 37.3726C31.3471 38.4692 24.6321 38.2161 21.5048 38.2161C18.3775 38.2161 11.6529 38.4598 8.83263 37.3726C6.95243 36.6416 5.5039 35.2265 4.74607 33.3802C3.6237 30.6155 3.88271 24.0552 3.88271 21C3.88271 17.9448 3.6333 11.3751 4.74607 8.61977C5.49431 6.78289 6.94283 5.36773 8.83263 4.62736C11.6625 3.53085 18.3775 3.78389 21.5048 3.78389C24.6321 3.78389 31.3567 3.54022 34.177 4.62736C36.0572 5.35836 37.5057 6.77351 38.2635 8.61977C39.3859 11.3845 39.1269 17.9448 39.1269 21C39.1269 24.0552 39.3859 30.6249 38.2635 33.3802Z" fill="white"/>
+                            </svg>
+                        </a>
+                        <a href="https://github.com/eucarlos" target="blank">
+                            <svg width="35" height="35" viewBox="0 0 46 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.3859 36.2339C15.3859 36.42 15.1726 36.5689 14.9036 36.5689C14.5976 36.5968 14.3843 36.4479 14.3843 36.2339C14.3843 36.0478 14.5976 35.8989 14.8665 35.8989C15.1448 35.871 15.3859 36.0199 15.3859 36.2339ZM12.5016 35.8152C12.4367 36.0013 12.6222 36.2153 12.9004 36.2712C13.1415 36.3642 13.4198 36.2712 13.4754 36.085C13.531 35.8989 13.3548 35.6849 13.0766 35.6012C12.8355 35.5361 12.5665 35.6291 12.5016 35.8152ZM16.6008 35.657C16.3319 35.7222 16.1464 35.8989 16.1742 36.113C16.202 36.2991 16.4431 36.42 16.7214 36.3549C16.9903 36.2898 17.1758 36.113 17.148 35.9269C17.1202 35.7501 16.8698 35.6291 16.6008 35.657ZM22.7032 0C9.83992 0 0 9.79824 0 22.7044C0 33.0237 6.47339 41.8542 15.7198 44.9621C16.9069 45.1761 17.3242 44.441 17.3242 43.8362C17.3242 43.2593 17.2964 40.0769 17.2964 38.1229C17.2964 38.1229 10.8044 39.5186 9.44113 35.3499C9.44113 35.3499 8.38387 32.6422 6.8629 31.9443C6.8629 31.9443 4.73911 30.4834 7.01129 30.5113C7.01129 30.5113 9.32056 30.6974 10.5911 32.912C12.6222 36.5038 16.0258 35.4709 17.352 34.8568C17.5653 33.368 18.1681 32.3351 18.8359 31.721C13.6516 31.1441 8.42097 30.3903 8.42097 21.4389C8.42097 18.88 9.12581 17.5959 10.6097 15.9582C10.3685 15.3534 9.58024 12.8596 10.8508 9.64005C12.7891 9.03522 17.25 12.1524 17.25 12.1524C19.1048 11.6313 21.0988 11.3615 23.0742 11.3615C25.0496 11.3615 27.0435 11.6313 28.8984 12.1524C28.8984 12.1524 33.3593 9.02592 35.2976 9.64005C36.5681 12.8689 35.7798 15.3534 35.5387 15.9582C37.0226 17.6052 37.9315 18.8893 37.9315 21.4389C37.9315 30.4183 32.469 31.1348 27.2847 31.721C28.1379 32.4561 28.8613 33.8518 28.8613 36.0385C28.8613 39.1743 28.8335 43.0545 28.8335 43.8176C28.8335 44.4224 29.2601 45.1575 30.4379 44.9435C39.7121 41.8542 46 33.0237 46 22.7044C46 9.79824 35.5665 0 22.7032 0ZM9.01452 32.0932C8.89395 32.1862 8.92177 32.4002 9.07944 32.577C9.22782 32.7259 9.44113 32.7911 9.56169 32.6701C9.68226 32.577 9.65444 32.363 9.49677 32.1862C9.34839 32.0373 9.13508 31.9722 9.01452 32.0932ZM8.0129 31.3395C7.94798 31.4604 8.04073 31.6093 8.22621 31.7024C8.3746 31.7954 8.56008 31.7675 8.625 31.6372C8.68992 31.5163 8.59718 31.3674 8.41169 31.2743C8.22621 31.2185 8.07782 31.2464 8.0129 31.3395ZM11.0177 34.6521C10.8694 34.773 10.925 35.0522 11.1383 35.229C11.3516 35.443 11.6206 35.4709 11.7411 35.322C11.8617 35.2011 11.806 34.9219 11.6206 34.7451C11.4165 34.5311 11.1383 34.5032 11.0177 34.6521ZM9.96048 33.2842C9.8121 33.3773 9.8121 33.6192 9.96048 33.8332C10.1089 34.0472 10.3593 34.1403 10.4798 34.0472C10.6282 33.9263 10.6282 33.6843 10.4798 33.4703C10.35 33.2563 10.1089 33.1633 9.96048 33.2842Z" fill="white"/>
+                            </svg>
+                        </a>
+                        <a href="https://dribbble.com/eucarlos" target="blank">                            
+                            <svg width="35" height="35" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.45625 18.3937C9.29062 14.4187 11.9625 11.1094 15.5531 9.42188C15.8906 9.88125 18.1781 13.0594 20.3063 16.8281C14.3063 18.4219 9.02812 18.4031 8.45625 18.3937ZM29.4938 11.4375C26.3438 8.64375 22.0594 7.58438 18 8.56875C18.3563 9.04688 20.6812 12.2156 22.7812 16.0687C27.3375 14.3531 29.2594 11.7656 29.4938 11.4375ZM13.1344 31.125C16.9313 34.0875 21.8812 34.5656 26.0062 32.8125C25.8187 31.6875 25.0687 27.7687 23.2687 23.1C18.1031 24.8625 14.475 28.3875 13.1344 31.125ZM22.3969 20.9813C22.0781 20.25 21.7219 19.5281 21.3563 18.8063C14.9625 20.7188 8.75625 20.6437 8.19375 20.625C8.19375 20.7563 8.18438 20.8875 8.18438 21.0188C8.18438 24.3094 9.43125 27.3094 11.475 29.5875C13.5563 26.0344 17.7656 22.2844 22.3969 20.9813ZM25.6687 22.5094C27.3469 27.1125 28.0219 30.8625 28.1531 31.6406C31.0312 29.7 33.075 26.6156 33.6469 23.0531C33.2156 22.9125 29.6812 21.8625 25.6687 22.5094ZM23.7656 17.9719C24.2156 18.8906 24.5438 19.6406 24.8906 20.4844C29.1562 19.95 33.3938 20.8031 33.8156 20.8969C33.7875 17.8687 32.7094 15.0938 30.9187 12.9188C30.6469 13.2844 28.5 16.0312 23.7656 17.9719ZM42 4.5V37.5C42 39.9844 39.9844 42 37.5 42H4.5C2.01562 42 0 39.9844 0 37.5V4.5C0 2.01562 2.01562 0 4.5 0H37.5C39.9844 0 42 2.01562 42 4.5ZM36 21C36 12.7313 29.2687 6 21 6C12.7313 6 6 12.7313 6 21C6 29.2687 12.7313 36 21 36C29.2687 36 36 29.2687 36 21Z" fill="white"/>
+                            </svg>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+            <h1 style="margin-top: .5rem;">LOREM IPSUM</h1>
+            <p>
+                Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed 
+                non enim. Maecenas lacinia non orci at aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus 
+                sapien, ut euismod leo tortor ac enim. In hac habitasse platea dictumst. Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit pretium sed non enim. Maecenas lacinia non orci at aliquam. Donec finibus, urna bibendum ultricies laoreet, augue eros luctus sapien, ut euismod leo tortor ac enim. In hac habitasse platea dictumst. Sed cursus venenatis tellus, non lobortis diam volutpat sit amet. Sed tellus augue, hendrerit eu rutrum in, porttitor at metus. Mauris ac hendrerit metus. Phasellus mattis lectus commodo felis egestas, id accumsan justo ultrices. Phasellus aliquet, sem a placerat dapibus, enim purus dictum lacus, nec ultrices ante dui ac ante. Phasellus placerat, urna.
+            </p>
+
+        </div>
+    </div>
+`
+
+const contentAbout = document.getElementById('ABOUT')
+contentAbout.innerHTML = ABOUT
